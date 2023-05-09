3146bd7d5212c59988a9d4ee071e26b05651cf8a
kruzikruz <kornel.kruzewski7@gmail.com>
2023-05-09 21:31:44 +0200

    1.002

--- API/Views/News.swift
+++ API/Views/News.swift
@@ -17,92 +17,93 @@ struct News: View {
     @EnvironmentObject var postsapicall: PostsAPICall
     
     var body: some View {
-        ScrollView{
-            VStack {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(0..<5) { _ in // Replace with your data model here
-                            Image("myImage")
-                                .renderingMode(.original)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: 320, maxHeight: 129)
-                                .clipped()
-                                .mask { RoundedRectangle(cornerRadius: 0, style: .continuous) }
-                                .overlay {
-                                    VStack(alignment: .leading) {
-                                        Text("I DRUŻYNA")
-                                            .padding(9)
-                                            .background {
-                                                RoundedRectangle(cornerRadius: 9, style: .continuous)
-                                                    .fill(.black)
-                                            }
-                                            .foregroundColor(.white)
-                                            .font(.system(size: 8, weight: .regular, design: .default))
-                                        Text("TRZY PUNKTY Z TERENU WICELIDERA  ")
-                                            .padding(5)
-                                            .foregroundColor(.white)
-                                            .font(.caption)
-                                        Text("5 LISTOPADA 2022 ")
-                                            .padding(9)
-                                            .foregroundColor(.white)
-                                            .font(.system(size: 6, weight: .regular, design: .default))
-                                    }
-                                    .padding(1)
-                                    .frame(maxWidth: .infinity)
+        NavigationView{
+            ScrollView(showsIndicators: false){
+                VStack {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0..<5) { _ in // Replace with your data model here
+                                Image("myImage")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: 320, maxHeight: 129)
                                     .clipped()
-                                }
+                                    .mask { RoundedRectangle(cornerRadius: 0, style: .continuous) }
+                                    .overlay {
+                                        VStack(alignment: .leading) {
+                                            Text("I DRUŻYNA")
+                                                .padding(9)
+                                                .background {
+                                                    RoundedRectangle(cornerRadius: 9, style: .continuous)
+                                                        .fill(.black)
+                                                }
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 8, weight: .regular, design: .default))
+                                            Text("TRZY PUNKTY Z TERENU WICELIDERA  ")
+                                                .padding(5)
+                                                .foregroundColor(.white)
+                                                .font(.caption)
+                                            Text("5 LISTOPADA 2022 ")
+                                                .padding(9)
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 6, weight: .regular, design: .default))
+                                        }
+                                        .padding(1)
+                                        .frame(maxWidth: .infinity)
+                                        .clipped()
+                                    }
+                            }
                         }
                     }
-                }
-                Image("STS")
-                    .renderingMode(.original)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipped()
-                    .padding()
-                ScrollView{
-                    NavigationView {
+                    Image("STS")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipped()
+                        .padding()
+                    ScrollView{
                         VStack {
-                            ForEach(postsapicall.posts, id: \.id) { posts in // Replace with your data model here
-                                NavigationLink(destination: NewsDetail(name: posts.title.rendered)) {
-                                    HStack {
-                                        Image("myImage")
-                                            .renderingMode(.original)
-                                            .resizable()
-                                            .frame(width: 160, height: 80)
-                                            .aspectRatio(contentMode: .fit)
-                                            .clipped();
-                                        VStack {
-                                            Text(posts.title.rendered)
-                                                .foregroundColor(.white)
-                                            HStack(alignment: .bottom) {
-                                                Text("Hello, World!")
-                                                    .background {
-                                                        RoundedRectangle(cornerRadius: 4, style: .continuous)
-                                                            .fill(Color(.systemFill))
-                                                    }
-                                                    .font(.system(size: 8, weight: .regular, design: .default))
-                                                Text(posts.date)
-                                                    .font(.system(size: 8, weight: .regular, design: .default))
+                            VStack {
+                                ForEach(postsapicall.posts, id: \.id) { posts in // Replace with your data model here
+                                    NavigationLink(destination: NewsDetail(name: posts.title.rendered)) {
+                                        HStack {
+                                            Image("myImage")
+                                                .renderingMode(.original)
+                                                .resizable()
+                                                .frame(width: 160, height: 80)
+                                                .aspectRatio(contentMode: .fit)
+                                                .clipped();
+                                            VStack {
+                                                Text(posts.title.rendered)
+                                                    .foregroundColor(.white)
+                                                HStack(alignment: .bottom) {
+                                                    Text("Hello, World!")
+                                                        .background {
+                                                            RoundedRectangle(cornerRadius: 4, style: .continuous)
+                                                                .fill(Color(.systemFill))
+                                                        }
+                                                        .font(.system(size: 8, weight: .regular, design: .default))
+                                                    Text(posts.date)
+                                                        .font(.system(size: 8, weight: .regular, design: .default))
+                                                }
+                                                .frame(maxWidth: .infinity, alignment: .bottom)
+                                                .clipped()
                                             }
-                                            .frame(maxWidth: .infinity, alignment: .bottom)
-                                            .clipped()
                                         }
                                     }
                                 }
                             }
-                        }
-                        .onAppear {
-                            postsapicall.getPosts()
+                            .onAppear {
+                                postsapicall.getPosts()
+                            }
                         }
                     }
                 }
-               }
-              }
-             }
             }
-        
+        }
+    }
+}
     struct News_Previews: PreviewProvider {
         static var previews: some View {
             News().environmentObject(PostsAPICall())
--- API/Views/NewsDetail.swift
+++ API/Views/NewsDetail.swift
@@ -8,64 +8,86 @@
 import SwiftUI
 
 struct NewsDetail: View {
-
-@EnvironmentObject var postsapicall: PostsAPICall
-let name: String
     
-var body: some View {
-        VStack {
-            VStack {
-                Text(name)
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject var postsapicall: PostsAPICall
+    let name: String
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                VStack {
+                    Text(name)
+                        .multilineTextAlignment(.leading)
+                        .padding(5)
+                }
+                .padding(.leading, 10)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .clipped()
+                HStack {
+                    Image("myImage")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    VStack {
+                        Text("Kornel Krużewski")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .clipped()
+                            .font(.system(size: 12, weight: .regular, design: .default))
+                        Text("Redaktor")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .clipped()
+                            .font(.system(size: 8, weight: .regular, design: .default))
+                    }
                     .multilineTextAlignment(.leading)
-                    .padding(5)
-            }
-            .padding(.leading, 10)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .clipped()
-            HStack {
+                    .frame(alignment: .leading)
+                    .clipped()
+                    VStack {
+                        Text("DATA")
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .clipped()
+                            .font(.system(size: 12, weight: .heavy, design: .default))
+                        Text("11:30")
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .clipped()
+                            .font(.system(size: 8, weight: .heavy, design: .default))
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .clipped()
+                    .multilineTextAlignment(.trailing)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
+                .clipped()
+                .padding()
+                .multilineTextAlignment(.leading)
                 Image("myImage")
                     .renderingMode(.original)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                VStack {
-                    Text("Kornel Krużewski")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .clipped()
-                        .font(.system(size: 12, weight: .regular, design: .default))
-                    Text("Redaktor")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .clipped()
-                        .font(.system(size: 8, weight: .regular, design: .default))
-                }
-                .multilineTextAlignment(.leading)
-                .frame(alignment: .leading)
-                .clipped()
-                VStack {
-                    Text("DATA")
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                        .clipped()
-                        .font(.system(size: 12, weight: .heavy, design: .default))
-                    Text("11:30")
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                        .clipped()
-                        .font(.system(size: 8, weight: .heavy, design: .default))
-                }
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .clipped()
-                .multilineTextAlignment(.trailing)
             }
-            .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
-            .clipped()
-            .padding()
-            .multilineTextAlignment(.leading)
-            Image("myImage")
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-        }
+        }.navigationBarTitle("News", displayMode: .inline)
+         .navigationBarBackButtonHidden(true)
+        
+         .toolbar {
+
+             // 2
+             ToolbarItem(placement: .navigationBarLeading) {
+                 Button {
+                     // 3
+                     dismiss()
+                 } label: {
+                     // 4
+                     HStack {
+
+                         Image(systemName: "chevron.backward")
+                             .foregroundColor(Color(.systemOrange))
+                     }
+                 }
+             }
+         }
+        
     }
 }
-
 struct NewsDetail_Previews: PreviewProvider {
     static var previews: some View {
         NewsDetail( name: "TEST").environmentObject(PostsAPICall())
